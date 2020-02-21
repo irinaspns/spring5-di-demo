96489a5f5e312ca4bcd54db26b403c9dba340123
zolotar <irinaspns@gmail.com>
2020-02-21 19:38:23 +0100

    classpath properties

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,12 +1,19 @@
 package guru.springframework;
 
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+//		SpringApplication.run(DiDemoApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
 	}
 }
